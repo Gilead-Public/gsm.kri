6038648f8b751ed04c35f646d88e5cc398ea0759
Jon Harmon <jon.harmon1@gilead.com>
2026-07-31 13:56:31 -0500

    More import cleanup

--- R/gsm.kri-package.R
+++ R/gsm.kri-package.R
@@ -2,12 +2,28 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import dplyr
+#' @import ggplot2
 #' @importFrom cli pluralize
 #' @importFrom cli qty
+#' @importFrom dplyr all_of
+#' @importFrom dplyr coalesce
+#' @importFrom dplyr desc
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr n_distinct
+#' @importFrom dplyr pull
+#' @importFrom dplyr select
+#' @importFrom dplyr summarize
+#' @importFrom dplyr ungroup
 #' @importFrom DT datatable
 #' @importFrom DT formatStyle
 #' @importFrom DT styleInterval
 #' @importFrom glue glue
+#' @importFrom gsm.core Flag
+#' @importFrom gsm.core LogMessage
 #' @importFrom htmltools h1
 #' @importFrom htmltools h2
 #' @importFrom htmltools h3
@@ -24,6 +40,8 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom jsonlite toJSON
 #' @importFrom lifecycle deprecated
+#' @importFrom purrr map
+#' @importFrom purrr map_dbl
 #' @importFrom rlang %||%
 #' @importFrom rlang .data
 #' @importFrom rlang check_installed
@@ -31,6 +49,8 @@
 #' @importFrom stats setNames
 #' @importFrom tibble enframe
 #' @importFrom tibble tibble
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr unnest
 #' @importFrom workr RunQuery
 ## usethis namespace: end
 NULL
