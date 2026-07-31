99a197274e8435299ddeb9d996b2c94d4c4a780e
Jon Harmon <jon.harmon1@gilead.com>
2026-07-31 14:07:41 -0500

    Revert import changes

--- R/gsm.kri-package.R
+++ R/gsm.kri-package.R
@@ -4,26 +4,16 @@
 ## usethis namespace: start
 #' @import dplyr
 #' @import ggplot2
-#' @importFrom cli pluralize
-#' @importFrom cli qty
-#' @importFrom dplyr all_of
-#' @importFrom dplyr coalesce
-#' @importFrom dplyr desc
-#' @importFrom dplyr filter
-#' @importFrom dplyr group_by
-#' @importFrom dplyr left_join
-#' @importFrom dplyr mutate
-#' @importFrom dplyr n_distinct
-#' @importFrom dplyr pull
-#' @importFrom dplyr select
-#' @importFrom dplyr summarize
-#' @importFrom dplyr ungroup
+#' @import gsm.core
+#' @import gt
+#' @import purrr
+#' @import tidyr
 #' @importFrom DT datatable
 #' @importFrom DT formatStyle
 #' @importFrom DT styleInterval
+#' @importFrom cli pluralize
+#' @importFrom cli qty
 #' @importFrom glue glue
-#' @importFrom gsm.core Flag
-#' @importFrom gsm.core LogMessage
 #' @importFrom htmltools h1
 #' @importFrom htmltools h2
 #' @importFrom htmltools h3
@@ -40,8 +30,6 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom jsonlite toJSON
 #' @importFrom lifecycle deprecated
-#' @importFrom purrr map
-#' @importFrom purrr map_dbl
 #' @importFrom rlang %||%
 #' @importFrom rlang .data
 #' @importFrom rlang check_installed
@@ -49,8 +37,5 @@
 #' @importFrom stats setNames
 #' @importFrom tibble enframe
 #' @importFrom tibble tibble
-#' @importFrom tidyr pivot_wider
-#' @importFrom tidyr unnest
-#' @importFrom workr RunQuery
 ## usethis namespace: end
 NULL
